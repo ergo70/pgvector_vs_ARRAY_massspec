5a1a963c1818111538575e3b9b60eb257497feb6
Ernst-Georg Schmid <333776+ergo70@users.noreply.github.com>
2024-03-31 22:34:27 +0200

    Update benchmark_table.sql

--- benchmark_table.sql
+++ benchmark_table.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION vector;
 
 CREATE TABLE spectra_index (
-	library_spectra_meta_id int4 not NULL,
-	mz float4[] not NULL,
-	intensity float4[] not null,
-	num_peaks int2 NULL,
-	mz_range numrange NULL,
-	mz_vector public.vector NULL
-);
+   library_spectra_meta_id int4 NOT NULL
+   num_peaks int2 NULL,
+   mz_range numrange NULL,
+   mz float4[] NOT NULL,
+   intensity float4[] NOT NULL,
+   mz_vector public.vector NULL,
+   intensity_vector public.vector NULL);
 
-CREATE INDEX spectra_index_num_peaks_idx ON massbase.spectra_index USING btree (num_peaks);
+CREATE INDEX spectra_index_num_peaks_idx ON spectra_index USING btree (num_peaks);
